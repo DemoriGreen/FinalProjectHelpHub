c80af160c805ff838213642275351d59e43fd933
DemoriGreen <demorigreens4@gmail.com>
2026-04-26 23:32:22 -0400

    Create errorHandler.js

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,9 @@
+function errorHandler(err, req, res, next) {
+  console.error(err);
+  res.status(500).json({
+    error: 'Internal server error',
+    details: process.env.NODE_ENV === 'development' ? err.message : undefined,
+  });
+}
+
+module.exports = errorHandler;
