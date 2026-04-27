070d3f09ed2ba7a8ce9d9d6886c8596c1ce328fe
DemoriGreen <demorigreens4@gmail.com>
2026-04-26 23:34:46 -0400

    Create User.js

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define(
+  'User',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2, 100],
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+  },
+  {
+    tableName: 'users',
+  }
+);
+
+module.exports = User;
